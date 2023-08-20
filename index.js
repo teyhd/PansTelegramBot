e0070bddfc140d99fb43c8605a7ed61051607974
root <vladteyhd@mail.ru>
2023-08-20 23:57:52 +0300

    PostAdd

--- index.js
+++ index.js
@@ -2,22 +2,38 @@ const PORT = process.env.PORT || 3334;
 const express = require('express');
 const app = express();
 const TelegramBot = require('node-telegram-bot-api');
-const token = '5709701051:AAEd99zSio5yiEgIXPDlbpUebqbkrZOrCpA';
+const token = '667589363:AAFZOOLBAqyWeFVjInlzU1X7_ySmrV7Iu8w';
 const bot = new TelegramBot(token, {polling: true});
 app.listen(PORT, ()=>{
     console.log('Телеграм запущен порт:', PORT)
  })
 
- app.get('/',async (req,res)=>{
-    if (req.query.msg!=undefined && req.query.num!=undefined) {
-        res.send('SEND')
-        let number = req.query.num;
-        let message = req.query.msg;
-        bot.sendMessage(`${number}`, message);
+app.get('/',async (req,res)=>{
+    if (req.query.msg!=undefined) {
+        let number = 304622290;
+        if (req.query.num!=undefined) {
+            number = req.query.num;
+        }
+        res.send('SENDING')
+        bot.sendMessage(`${number}`, req.query.msg);
     } else{
-        res.send('NO' +req.query)
+        res.send('NO: ' +req.query)
     }
 })
+//kode
+app.post('/',async (req,res)=>{
+    //console.log(req.body);
+    console.log(req.headers);
+    let number = 304622290;
+   // bot.sendMessage(`${number}`, req.body);
+    for (key in req.headers) {
+        bot.sendMessage(`${number}`, `${key}:${req.headers[key]}`);
+    }
+    
+    
+})
+
+
 bot.sendMessage(304622290, "Телеграм бот - запущен");
 
 http://localhost:3334?msg=hi&num=304622290
